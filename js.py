b02b87f082efd77e338aac9f33c66d91524b5f70
Zan-0xy <ahmadsan272@gmail.com>
2026-02-04 14:58:56 +0700

    Update js.py

--- js.py
+++ js.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python
 from multiprocessing import Process, Manager
+impor urllib3 
 from urllib.parse import urlparse, ssl
 import sys, getopt, random, time
+import requests
 
 # Python version-specific 
 if  sys.version_info < (3,0):
